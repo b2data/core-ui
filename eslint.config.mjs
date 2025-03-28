b06395b54412f7dd83e36633c1b74ac29e5125d7
Nikolai Turov <turov96@yandex.ru>
2025-03-29 00:57:17 +0300

    [1.1.9] Patch dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import globals from "globals";
+// import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
@@ -6,7 +6,7 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  // { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
