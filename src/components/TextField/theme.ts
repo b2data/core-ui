38d5b23d946b0d5bf534f86522013ce75b51005f
Nikolai Turov <turov96@yandex.ru>
2025-03-31 13:10:29 +0300

    [1.2.2] Fixed styles

--- src/components/TextField/theme.ts
+++ src/components/TextField/theme.ts
@@ -72,6 +72,7 @@ const textFieldTheme: ComponentThemeOverride["MuiTextField"] = {
           "& .MuiOutlinedInput-root": {
             backgroundColor: "#fafbfc",
             marginTop: "5px",
+            height: 35,
             minHeight: 35,
             padding: 0,
             "& .MuiOutlinedInput-notchedOutline": {
